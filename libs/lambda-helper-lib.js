2c96e9e1c67e7228c40e931d7630aa6335e733f5
Chinmay Phulse <phulsechinmay@gmail.com>
2019-06-13 23:49:32 -0700

    Fix for mongoose. Minor changes

--- libs/lambda-helper-lib.js
+++ libs/lambda-helper-lib.js
@@ -1,15 +1,19 @@
 import { badRequest } from './response-lib';
 
+// Verify that request coming to AWS lambda contains the needed keys
 export function apiVerificationDecorator(primaryKey, keysToCheck, lambdaFunc) {
     return async function (event, context) {
         var mapToCheck = event;
         if (primaryKey != "") {
             mapToCheck = event[primaryKey]
+            if (mapToCheck == null) {
+                return badRequest({'error': `Required key(s) not present in query: [${keysToCheck.join(", ")}]`})
+            }
         }
         // Check all required keys exist in map
         for (var keyInd in keysToCheck) {
             if (!(keysToCheck[keyInd] in mapToCheck)) {
-                return badRequest({'error': `Required key not present in query: \'${keysToCheck[keyInd]}\'`})
+                return badRequest({'error': `Required key not present in query: ${keysToCheck[keyInd]}`})
             }
         }
         return await lambdaFunc(event, context);
