4c4ef1547a7ddbdd4050a1ceca36923fac48fdd0
Chinmay Phulse <phulsechinmay@gmail.com>
2019-06-18 22:29:04 -0700

    Added description for CRUD base files

--- lambdas/crud/deleteBase.js
+++ lambdas/crud/deleteBase.js
@@ -1,5 +1,8 @@
 import * as dynamoDbLib from '../../libs/dynamodb-lib';
 
+// Base version of CRUD DELETE
+// Only returns items instead of lambda return values
+
 export default (params) =>
     new Promise((resolve) =>
         dynamoDbLib
