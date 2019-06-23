3071d9fed65eb908da99b5e0f5a8a2decb420fe5
Josiah Coad <josiah@coad.net>
2019-06-23 14:27:15 -0700

    changing update.js back to what it was before

--- lambdas/crud/update.js
+++ lambdas/crud/update.js
@@ -5,18 +5,8 @@ export default (params) =>
     new Promise((resolve) =>
         dynamoDbLib
             .call('update', params)
-            .then(() =>
-                resolve(
-                    success({
-                        data: {},
-                        errors: [],
-                    }),
-                ))
-            .catch(({ message }) => {
-                resolve(
-                    failure({
-                        data: {},
-                        errors: [message],
-                    }),
-                );
+            .then(() => resolve(success({ status: true })))
+            .catch((err) => {
+                console.log(err.message);
+                resolve(failure({ status: false }));
             }));
